e97b7b03752c9f7dd91419fdf750add62009aa02
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2018-03-31 14:01:31 -0600

    Checkpoint:  fixing queue policy

--- aws/message_queue.py
+++ aws/message_queue.py
@@ -13,9 +13,12 @@ class MessageQueue:
         self.topic = self.sns.create_topic(Name=LFDE_SNS_TOPIC)
         self.topic_arn = self.topic.arn
         self.sqs = boto3.resource(SQS)
-        self.queue_attributes = MESSAGE_QUEUE_ATTRIBUTES
-        self.queue_attributes[POLICY] = create_write_from_sns_policy(self.topic_arn)
-        self.queue = self.sqs.create_queue(QueueName=queue_name, Attributes=self.queue_attributes)
+        self.sqs_client = boto3.client(SQS)
+        self.queue = self.sqs.create_queue(QueueName=queue_name)
+        queue_attributes = MESSAGE_QUEUE_ATTRIBUTES
+        queue_attributes[POLICY] = MessageQueue.create_write_from_sns_policy(self.queue.attributes[QUEUE_ARN])
+        self.sqs_client.set_queue_attributes(QueueUrl=self.queue.url, Attributes=queue_attributes)
+        self.queue.Attributes = queue_attributes
         self.subscription = self.topic.subscribe(Protocol=SQS, Endpoint=self.topic_arn)
 
     def __del__(self):
@@ -25,22 +28,30 @@ class MessageQueue:
         return self.queue.attributes[QUEUE_ARN]
 
     @staticmethod
-    def create_write_from_sns_policy(topicarn):
-        policy_document = """{{
-          "Version":"2012-10-17",
-          "Statement":[
-            {{
-              "Sid":"MyPolicy",
-              "Effect":"Allow",
-              "Principal" : {{"AWS" : "*"}},
-              "Action":"SQS:SendMessage",
-              "Resource": "{}",
-            }}
-          ]
-        }}""".format(topicarn)
+    def create_write_from_sns_policy(queue_arn):
+        policy_document = """{
+  "Id": "Policy1522525396010",
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Stmt1522525389948",
+      "Action": [
+        "sqs:SendMessage"
+      ],
+      "Effect": "Allow",
+      "Resource": {}
+      "Principal": {
+        "AWS": [
+          "arn:aws:sns:us-west-2:856259575263:liquid_fortress_distributed_election"
+        ]
+      }
+    }
+  ]
+}""".format(queue_arn)
+        print(policy_document)
         return policy_document
 
-    def send_message(self, msg):
+    def send_message(self, message):
         logging.debug("Publishing message: {}".format(message))
         result = self.topic.publish(Message=message)
         logging.debug("Publish result: {}".format(result))
@@ -48,11 +59,11 @@ class MessageQueue:
 
     def receive_message(self):
         message_list = self.queue.receive_messages()
-        if len(message_list) > 0:
-            message = message_list[0]
-            body = message.body
-            message_id = message.message_id
-            logging.debug("Received message: \'{}\' with message_id: {}".format(body, message_id))
-            message.delete()
-            return body
-
+        if len(message_list) == 0:
+            return None
+        message = message_list[0]
+        body = message.body
+        message_id = message.message_id
+        logging.debug("Received message: \'{}\' with message_id: {}".format(body, message_id))
+        message.delete()
+        return body
